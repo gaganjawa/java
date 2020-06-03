583610bb7e322254fde1b1001fb31dc9d769280e
Gagan <jawagag@gmail.com>
2020-06-03 22:08:16 +0530

    Added sorting in stack

--- DataStructures/src/stackandqueue/Stack.java
+++ DataStructures/src/stackandqueue/Stack.java
@@ -1,9 +1,10 @@
 package stackandqueue;
 
-public class Stack <V> {
+public class Stack<V> {
     private int maxSize;
     private int top;
-    private V array[];
+    private V[] array;
+    private int currentSize;
 
     /*
     Java does not allow generic type arrays. So we have used an
@@ -16,6 +17,11 @@ public class Stack <V> {
         this.maxSize = max_size;
         this.top = -1; //initially when stack is empty
         array = (V[]) new Object[max_size];//type casting Object[] to V[]
+        this.currentSize = 0;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
     }
 
     //returns the maximum size capacity
@@ -24,36 +30,37 @@ public class Stack <V> {
     }
 
     //returns true if Stack is empty
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return top == -1;
     }
 
     //returns true if Stack is full
-    public boolean isFull(){
-        return top == maxSize -1;
+    public boolean isFull() {
+        return top == maxSize - 1;
     }
 
     //returns the value at top of Stack
-    public V top(){
-        if(isEmpty())
+    public V top() {
+        if (isEmpty())
             return null;
         return array[top];
     }
 
     //inserts a value to the top of Stack
-    public void push(V value){
-        if(isFull()) {
+    public void push(V value) {
+        if (isFull()) {
             System.err.println("Stack is Full!");
             return;
         }
         array[++top] = value; //increments the top and adds value to updated top
+        currentSize++;
     }
 
     //removes a value from top of Stack and returns
-    public V pop(){
-        if(isEmpty())
+    public V pop() {
+        if (isEmpty())
             return null;
+        currentSize--;
         return array[top--]; //returns value and top and decrements the top
     }
-
 }
--- DataStructures/src/util/ArrayPractice.java
+++ DataStructures/src/util/ArrayPractice.java
@@ -1,10 +1,5 @@
 package util;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class ArrayPractice {
 	
 	public static void main(String[] args) {
